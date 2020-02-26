49c84b7e4780843de0556ac46a05abffe0d8f9f2
eralbrecht <60952785+eralbrecht@users.noreply.github.com>
2020-02-25 20:41:32 -0800

    Add files via upload

--- RoseAlbrechtDNA.cpp
+++ RoseAlbrechtDNA.cpp
@@ -9,6 +9,7 @@ int main()
 	ofstream outputfile;
 	outputfile.open("rosealbrechtoutput.txt" , ios::out | ios::app);
 	outputfile << ""<<endl;
+	outputfile<< "RoseAlbrecht 2300456"<<"\n";
 	while (analyze)
 	{
 		//take in the name of the file were reading, open and initiate both input and output files
@@ -19,35 +20,37 @@ int main()
 		ifstream inputfile;
 		inputfile.open("filename.txt");
 		//initialize counting variables
-		int lineCount = 1;
+		int lineCount;
 		int aCount;
 		int cCount;
 		int tCount;
 		int gCount;
-		int totalCount = 1;
+		int totalCount;
 		string str;
 		//count the number of each nucleotide as well as number of lines in input file and number of total characters in input file
 		while (getline(inputfile, str))
 		{
 			lineCount +=1;
 			//iterate through characters in lines
-			for (std::string::size_type i = 0; i < str.size(); i++)
+			cout<<"made it into while loop";
+			for (char letter : str)
 			{
+				cout<<"made it into for loop";
 				totalCount +=1;
-				toupper(str[i]);
-				if (str[i] == 'A')
+				toupper(letter);
+				if (letter == 'A')
 				{
 					aCount+=1;
 				}
-				if (str[i] == 'C')
+				if (letter == 'C')
 				{
 					cCount+=1;
 				}
-				if (str[i] == 'T')
+				if (letter == 'T')
 				{
 					tCount+=1;
 				}
-				if (str[i] == 'G')
+				if (letter == 'G')
 				{
 					gCount+=1;
 				}
@@ -188,42 +191,49 @@ int main()
 		int gtRelative = 100*gtCount/bigramTotal;
 		int ggRelative = 100*ggCount/bigramTotal;
 		cout << "line190"<<"\n";
-		outputfile << "Input Statistics: /n"<<endl;
-		outputfile << "Sum of DNA string lengths: " <<totalCount << "/n"<<endl;
-		outputfile << "Mean string length: " << lineMean << " /n"<<endl;
-		outputfile << "Variance of string length: " << lineVariance << " /n"<<endl;
-		outputfile << "Standard deviation of string length: " << stdDev << " /n"<<endl;
-		outputfile << "Relative probability of nucleotide A: " << aRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide C: " << cRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide T: " << tRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide G: " << gRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram AA: " << aaRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram AC: " << acRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram AT: " << atRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram AG: " << agRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram CA: " << caRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram CC: " << ccRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram CT: " << ctRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram CG: " << cgRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram TA: " << taRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram TC: " << tcRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram TT: " << ttRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram TG: " << tgRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram GA: " << gaRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram GC: " << gcRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram GT: " << gtRelative << "% /n"<<endl;
-		outputfile << "Relative probability of nucleotide bigram GG: " << ggRelative << "% /n"<<endl;
+		outputfile << "Input Statistics: "<<"\n"<<endl;
+		outputfile << "a count value: "<<aCount<<endl;
+		outputfile << "c count value: "<<cCount<<endl;
+		outputfile << "t count value: "<<tCount<<endl;
+		outputfile << "g count value: "<<gCount<<endl;
+		outputfile << "Sum of DNA string lengths: " <<totalCount << "\n"<<endl;
+		outputfile << "Mean string length: " << lineMean << " \n"<<endl;
+		outputfile << "Variance of string length: " << lineVariance << " \n"<<endl;
+		outputfile << "Standard deviation of string length: " << stdDev << " \n"<<endl;
+		outputfile << "Relative probability of nucleotide A: " << aRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide C: " << cRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide T: " << tRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide G: " << gRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram AA: " << aaRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram AC: " << acRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram AT: " << atRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram AG: " << agRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram CA: " << caRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram CC: " << ccRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram CT: " << ctRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram CG: " << cgRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram TA: " << taRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram TC: " << tcRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram TT: " << ttRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram TG: " << tgRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram GA: " << gaRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram GC: " << gcRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram GT: " << gtRelative << "%" << "\n"<<endl;
+		outputfile << "Relative probability of nucleotide bigram GG: " << ggRelative << "%" << "\n"<<endl;
 		cout << "line215"<<"\n";
 		//need to add all the information that needs to be printed
 		//create 1000 lines of dna
 		for(int i=0; i<1000; i++)
 		{
-			double A = 1*rand()/RAND_MAX;
-			double B = 1*rand()/RAND_MAX;
+			double A = 1.0*rand()/RAND_MAX;
+			double B = 1.0*rand()/RAND_MAX;
 			double C = sqrt(-2*log(A))*cos(2*3.14159*B);
 			double D = stdDev + C + lineMean;
 			double letterDecision;
-			//make letterDecision = a number between 0 and 100 0 inclusive 100 exclusive
+			if (D<0)//i want to make sure our d value isnt negative because it makes an infinite ammount of T's print...
+			{
+				D = D*-1;
+			}
 			for (int charPrint=0; charPrint<D; charPrint++)
 			{
 				if (letterDecision < aRelative)
@@ -236,7 +246,6 @@ int main()
 				}
 				else if (letterDecision < aRelative + cRelative + tRelative)
 				{
-					outputfile<<"test";
 					outputfile << "T";
 				}
 				else
@@ -244,10 +253,9 @@ int main()
 					outputfile << "G";
 				}
 			}
-			cout <<"\n";
 		}
 		cout << "line250"<<"\n";
-		cout << "would you like to run another file?"<<"\n";
+		cout << "would you like to run another file (y or n)?"<<"\n";
 		char continueReading;
 		cin >> continueReading;
 		if (continueReading == 'n')
